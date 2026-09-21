27ba1dc38cdecd773dff26a3ad99ac01e30efda1
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2026-09-21 13:44:10 -0300

    feat : adicionado ao server (helmet,cors,rate limit)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,37 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
 import {httpLogger} from './shared/logger/httpLogger.js';
 import {errorHandler} from './shared/errors/errorHandler.js';
 import {prisma} from './shared/database/prisma.js';
 
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit : 100, // limit each IP to 100 requests per windowMs
+    standardHeaders : true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders : false, // Disable the `X-RateLimit-*` headers
+    message : {
+        success : false,
+        error : {
+            code : "TOO_MANY_REQUESTS",
+            message : "Too many requests, please try again later."
+        }
+    }
+
+
+})
+
+app.use(helmet());
+app.use(
+    cors({
+        origin : "*",
+    })
+)
+app.use(limiter);
 
 app.use(httpLogger);
 app.use(express.json());
