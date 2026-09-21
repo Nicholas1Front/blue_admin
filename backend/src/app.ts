21283794ed9263ae31ff90d69687fec381a261a1
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2026-09-21 12:27:23 -0300

    feat : prisma adapter configurado aguardando testes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {httpLogger} from './shared/logger/httpLogger.js';
 import {errorHandler} from './shared/errors/errorHandler.js';
+import {prisma} from './shared/database/prisma.js';
 
 const app = express();
 
@@ -11,6 +12,15 @@ app.get('/health', (_req,res)=>{
     res.json({
         success : true,
         message : "API is running"
+    })
+})
+
+app.get('/health/db', async (_req,res)=>{
+    await prisma.$queryRaw`SELECT 1`;
+
+    res.json({
+        success : true,
+        message : "Database is connected"
     })
 })
 
